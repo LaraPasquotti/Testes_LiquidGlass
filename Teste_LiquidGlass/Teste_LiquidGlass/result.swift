de5e32708a980e70a45b639a5518c86ba3e44471
Lara Matias Pasquotti <laramatias@MacBook-Pro-Lara.local>
2025-09-23 11:16:19 -0300

    Criando um botão que leva pra outra tela

---
+++ Teste_LiquidGlass/Teste_LiquidGlass/result.swift
@@ -0,0 +1,19 @@
+//
+//  result.swift
+//  Teste_LiquidGlass
+//
+//  Created by Lara Matias Pasquotti on 23/09/25.
+//
+
+import SwiftUI
+
+struct result: View {
+    var body: some View {
+        Text("Olá")
+            .font(Font.largeTitle.bold())
+    }
+}
+
+#Preview {
+    result()
+}
